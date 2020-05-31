40c333c354e6cc42309cec4aa25166c04707f239
Venkata Sudhakar <venkatasudhakar33@gmail.com>
2020-05-31 11:13:28 +0530

    CoreData with swiftUI example

--- SwiftUICoreData/ContentView.swift
+++ SwiftUICoreData/ContentView.swift
@@ -9,13 +9,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert:Bool = false
+    @State private var showUpdateAlert:Bool = false
+    @Environment(\.managedObjectContext) var moc
+    @State private var selectedemployee:Employee?
+    @State private var name:String = ""
+    @FetchRequest(entity: Employee.entity(), sortDescriptors: []) var employees:FetchedResults<Employee>
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List{
+                ForEach(employees,id: \.id){ employee in
+                    Text(employee.name ?? "")
+                        .onTapGesture {
+                            withAnimation {
+                                self.selectedemployee = employee
+                                self.showUpdateAlert.toggle()
+                            }
+                    }
+                }
+                .onDelete(perform: deleteEmployee(at:))
+                
+            }
+            .navigationBarTitle("Empoyees",displayMode: .inline)
+            .navigationBarItems(trailing:
+                Button(action: {
+                    //action add
+                    withAnimation {
+                        self.showAlert.toggle()
+                    }
+                    
+                }, label: {
+                    Image(systemName: "plus.square.fill")
+                        .font(.system(size: 30))
+                })
+                
+            )
+                .navigationBarItems(leading:
+                    EditButton()
+                    , trailing:  Button(action: {
+                        //action add
+                        withAnimation {
+                            self.showAlert.toggle()
+                        }
+                        
+                    }, label: {
+                        Image(systemName: "plus.square.fill")
+                            .font(.system(size: 30))
+                    })
+            )
+        }
+        .alert(isPresented: $showAlert, TextAlert(title: "Add Employee",placeholder: "Enter name", action: {
+            if let name = $0 {
+                self.addNewEmployee(name: name)
+            }
+        }))
+            .alert(isPresented: $showUpdateAlert, TextAlert(title: "Update Employee",value: selectedemployee?.name ?? "", action: {
+                if let name = $0 {
+                    self.updateEmployee(emp: self.selectedemployee!, name: name)            }
+            }))
+        
+    }
+    func addNewEmployee(name:String){
+        let employee = Employee(context: moc)
+        employee.id = UUID()
+        employee.name = name
+        try? moc.save()
+    }
+    func deleteEmployee(at offsets:IndexSet){
+        for index in offsets {
+            let emp = employees[index]
+            moc.delete(emp)
+            try? moc.save()
+        }
+    }
+    func updateEmployee(emp:Employee,name:String){
+        emp.name = name
+        try? moc.save()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
